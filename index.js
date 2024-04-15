50fb00d6e821f24faec190538da706ad2ea84b59
Ayush <ayush3032@gmail.com>
2024-04-16 00:12:54 +0530

    create basic routers

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello Ayush!')
-})
+// available routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`)

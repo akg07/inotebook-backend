07ae25d6977d3f2afc48da4cc7ef4e5bce2c3b17
Ayush <ayush3032@gmail.com>
2024-04-17 00:16:50 +0530

    login endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -48,10 +48,53 @@ router.post('/create-user', [
     res.send({authToken, message: "User created successfully"})
   }catch(ex) {
     console.error(ex.message);
-    res.status(500).send("some error occured.");
+    res.status(500).send("Internal server error");
   }
   
 });
 
+// Authenticate a User "/api/auth/login". 
+
+router.post('/login', [
+  body('email', 'Enter a valid email').isEmail(),
+  body('password', 'Password cannot be blank').exists(),
+], async (req, res)=>{
+  
+  //  If error comes return it
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() });
+  }
+
+  const {email, password} = req.body;
+  
+  try{
+    let user = await User.findOne({email});
+
+    if(!user) {
+      return res.status(400).json({error: "Incorrect email/password"});
+    }
+
+    const passwordCompare = await bcrypt.compare(password, user.password);
+
+    if(!passwordCompare) {
+      return res.status(400).json({error: "Incorrect email/password"});
+    }
+
+    const data = {
+      user : {
+        id: user._id
+      }
+    }
+
+    const authToken = jwt.sign(data, JWT_SECERT);
+
+    res.send({authToken, message: "Login successfully"});
+
+  }catch(error) {
+    console.error(ex.message);
+    res.status(500).send("Internal server error");
+  }
+});
 
 module.exports = router;

18a43043a5d081acd3c3045640401deec5344a38
Ayush <ayush3032@gmail.com>
2024-04-17 01:06:02 +0530

    update-note endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -17,12 +17,10 @@ router.get('/fetch-all-notes', fetchuser, async (req, res) => {
   }
 })
 
-// Add a new notes using post : GET "api/notes/fetch-all-notes" : Login required
+// Add a new notes using post : GET "api/notes/add-note" : Login required
 router.post('/add-note', fetchuser, [
   body('title', 'Enter a valid Title').isLength({min: 3}),
-  body('description', 'Enter a valid description').isLength({min: 5}),
-  body('title'),
-
+  body('description', 'Enter a valid description').isLength({min: 5})
 ], async (req, res) => {
   try{
     //  If error comes return it
@@ -46,5 +44,34 @@ router.post('/add-note', fetchuser, [
   }
 })
 
+// Update a notes using post : GET "api/notes/update-note" : Login required
+router.post('/update-note/:id', fetchuser, async (req, res) => {
+  try{
+    const {title, description, tag} = req.body;
+
+    // CREATE A NEW NOTE OBJECT
+    const newNote = {};
+    if(title) {newNote.title = title};
+    if(description) {newNote.description = description};
+    if(tag) {newNote.tag = tag};
+
+    // Find the note to be updated and update it
+    let note = await Notes.findById(req.params.id);
+    if(!note) {
+      res.status(404).send("Note Not Found");
+    }
+
+    if(note.user.toString() !== req.user.id) {
+      res.status(401).send("Unauthorized Note access");
+    }
+
+    note = await Notes.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+    res.send({updatedNote: note, message: "Notes Updated Successfully"});
+    
+  }catch(error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 module.exports = router;

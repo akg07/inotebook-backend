e5515c0106e6fef7febdc0b48c5dcccb61f50923
Ayush <ayush3032@gmail.com>
2024-04-17 00:03:13 +0530

    update DB

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
+const mongoURI = "mongodb://localhost:27017/inotebook";
 
 mongoose.connect(mongoURI);
 
@@ -8,7 +8,7 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error.'));
 
 db.once('open', ()=> {
-  console.log('Connected to MongoDB successfully.');
+  console.log('Connected to inotebook MongoDB successfully.');
 });
 
 module.exports = db;

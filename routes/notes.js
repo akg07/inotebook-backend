794fc8c93b0bf326931159aa4a491b44e7a68873
Ayush <ayush3032@gmail.com>
2024-04-17 00:42:29 +0530

    get all notes endpoint

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const Notes = require('../models/Notes');
+const fetchuser = require('../middleware/fetchuser');
 
+
+// Get all the notes : GET "api/notes/fetch-all-notes"
+router.get('/fetch-all-notes', fetchuser, async (req, res) => {
+  try{
+    const notes = await Notes.find({user: req.user.id});
+    res.json(notes);
+  }catch(error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+})
 
 
 module.exports = router;
